798a2461be018de8666c24ec3a329ea123589573
open_mailbox <games@open-mailbox.com>
2018-07-27 12:40:45 -0400

    Move auction updating behavior into its own file.

---
+++ auctions.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/lib/pq"
+)
+
+type AuctionDumpResponse struct {
+	Files []AuctionDumpFile `json:"files"`
+}
+
+type AuctionDumpFile struct {
+	Url          string `json:"url"`
+	LastModified int    `json:"lastmodified"`
+}
+
+type AuctionRealm struct {
+	Name string `json:name`
+	Slug string `json:slug`
+}
+
+type AuctionFetchResponse struct {
+	Realms   []AuctionRealm `json:realms`
+	Auctions []Auction      `json:auctions`
+}
+
+type Auction struct {
+	Auc        int    `json:auc`
+	Item       int    `json:item`
+	Owner      string `json:owner`
+	OwnerRealm string `json:ownerRealm`
+	Bid        int    `json:bid`
+	Buyout     int    `json:buyout`
+	Quantity   int    `json:quantity`
+	TimeLeft   string `json:timeLeft`
+	Rand       int    `json:rand`
+	Seed       int    `json:seed`
+	Context    int    `json:context`
+}
+
+func refreshAuctions(db *sql.DB, api_key string) {
+	var fileId int
+
+	deleteExisting(db)
+
+	latest := loadLatest(db)
+	files := fetchDumps(api_key)
+
+	for _, file := range files {
+		if file.LastModified > latest {
+			auctions := fetchAuctions(file)
+
+			createAuctions(db, auctions)
+
+			err := db.QueryRow(`INSERT INTO auction_files (url, last_modified) VALUES ($1, $2) RETURNING id`,
+				file.Url, file.LastModified).Scan(&fileId)
+			checkError(err)
+		} else {
+			log.Printf("Skipping. File too old: %v\n", file.Url)
+		}
+	}
+
+}
+
+func createAuctions(db *sql.DB, auctions []Auction) {
+	txn, err := db.Begin()
+	checkError(err)
+
+	stmt, err := txn.Prepare(pq.CopyIn(
+		"auctions",
+		"auction_id",
+		"item_id",
+		"owner",
+		"owner_realm",
+		"bid",
+		"buyout",
+		"quantity",
+		"time_left",
+		"rand",
+		"seed",
+		"context"))
+
+	checkError(err)
+
+	for _, auction := range auctions {
+		_, err = stmt.Exec(
+			auction.Auc,
+			auction.Item,
+			auction.Owner,
+			auction.OwnerRealm,
+			auction.Bid,
+			auction.Buyout,
+			auction.Quantity,
+			auction.TimeLeft,
+			auction.Rand,
+			auction.Seed,
+			auction.Context)
+
+		checkError(err)
+	}
+
+	_, err = stmt.Exec()
+	checkError(err)
+
+	err = stmt.Close()
+	checkError(err)
+
+	err = txn.Commit()
+	checkError(err)
+}
+
+func deleteExisting(db *sql.DB) {
+	log.Println("Deleting previous auction data.")
+	_, err := db.Query(`DELETE FROM auctions`)
+	checkError(err)
+}
+
+func fetchDumps(api_key string) []AuctionDumpFile {
+	log.Println("Fetching auction dump files...")
+
+	resp, err := http.Get("https://us.api.battle.net/wow/auction/data/archimonde?locale=en_US&apikey=" + api_key)
+	checkError(err)
+
+	var data AuctionDumpResponse
+	decoder := json.NewDecoder(resp.Body)
+
+	err = decoder.Decode(&data)
+	checkError(err)
+
+	log.Printf("Retrieved %v files.\n", len(data.Files))
+
+	return data.Files
+}
+
+func fetchAuctions(file AuctionDumpFile) []Auction {
+	log.Printf("Fetching from %v\n", file.Url)
+
+	resp, err := http.Get(file.Url)
+	if err != nil {
+		log.Printf(err.Error())
+		log.Println("\nSkipping file.")
+		return make([]Auction, 0)
+	}
+
+	var data AuctionFetchResponse
+	decoder := json.NewDecoder(resp.Body)
+
+	err = decoder.Decode(&data)
+	if err != nil {
+		log.Printf(err.Error())
+		log.Println("\nSkipping file.")
+		return make([]Auction, 0)
+	}
+
+	return data.Auctions
+}
+
+func loadLatest(db *sql.DB) int {
+	var latest int
+
+	rows, err := db.Query(`SELECT last_modified FROM auction_files ORDER BY last_modified DESC LIMIT 1`)
+	checkError(err)
+
+	defer rows.Close()
+	for rows.Next() {
+		err = rows.Scan(&latest)
+		checkError(err)
+	}
+
+	err = rows.Err()
+	checkError(err)
+
+	log.Printf("Latest file is %v\n", latest)
+
+	return latest
+}
--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"log"
-	"net/http"
 	"os"
-
-	"github.com/lib/pq"
 )
 
 const DbConnnect = "dbname=wow host=/run/postgresql"
-
-var db *sql.DB
 
 func checkError(err error) {
 	if err != nil {
@@ -20,115 +14,30 @@ func checkError(err error) {
 	}
 }
 
-func deleteExisting() {
-	log.Println("Deleting previous auction data.")
-	_, err := db.Query(`DELETE FROM auctions`)
-	checkError(err)
-}
+func loadItemIds(db *sql.DB) []int {
+	var count int
 
-func fetchAuctions(file AuctionDumpFile) {
-	log.Printf("Fetching from %v\n", file.Url)
-
-	resp, err := http.Get(file.Url)
-	if err != nil {
-		log.Printf(err.Error())
-		log.Println("\nSkipping file.")
-		return
-	}
-
-	var data AuctionFetchResponse
-	decoder := json.NewDecoder(resp.Body)
-
-	err = decoder.Decode(&data)
-	if err != nil {
-		log.Printf(err.Error())
-		log.Println("\nSkipping file.")
-		return
-	}
-
-	txn, err := db.Begin()
+	err := db.QueryRow(`SELECT count(DISTINCT item_id) FROM auctions`).Scan(&count)
 	checkError(err)
 
-	stmt, err := txn.Prepare(pq.CopyIn(
-		"auctions",
-		"auction_id",
-		"item_id",
-		"owner",
-		"owner_realm",
-		"bid",
-		"buyout",
-		"quantity",
-		"time_left",
-		"rand",
-		"seed",
-		"context"))
+	ids := make([]int, count)
+	i := 0
 
+	rows, err := db.Query(`SELECT DISTINCT item_id FROM auctions`)
 	checkError(err)
 
-	for _, auction := range data.Auctions {
-		_, err = stmt.Exec(
-			auction.Auc,
-			auction.Item,
-			auction.Owner,
-			auction.OwnerRealm,
-			auction.Bid,
-			auction.Buyout,
-			auction.Quantity,
-			auction.TimeLeft,
-			auction.Rand,
-			auction.Seed,
-			auction.Context)
-
-		checkError(err)
-	}
-
-	_, err = stmt.Exec()
-	checkError(err)
-
-	err = stmt.Close()
-	checkError(err)
-
-	err = txn.Commit()
-	checkError(err)
-}
-
-func fetchDumps(api_key string) []AuctionDumpFile {
-	log.Println("Fetching auction dump files...")
-
-	resp, err := http.Get("https://us.api.battle.net/wow/auction/data/archimonde?locale=en_US&apikey=" + api_key)
-	checkError(err)
-
-	var data AuctionDumpResponse
-	decoder := json.NewDecoder(resp.Body)
-
-	err = decoder.Decode(&data)
-	checkError(err)
-
-	log.Printf("Retrieved %v files.\n", len(data.Files))
-
-	return data.Files
-}
-
-func loadLatest() int {
-	var latest int
-
-	rows, err := db.Query(`SELECT last_modified FROM auction_files ORDER BY last_modified DESC LIMIT 1`)
-	checkError(err)
-
+	defer rows.Close()
 	for rows.Next() {
-		err = rows.Scan(&latest)
-		checkError(err)
+		err = rows.Scan(&ids[i])
 	}
 
-	log.Printf("Latest file is %v\n", latest)
+	err = rows.Err()
+	checkError(err)
 
-	return latest
+	return ids
 }
 
 func main() {
-	var err error
-	var fileId int
-
 	api_key := os.Getenv("BLIZZARD_API_KEY")
 
 	if len(api_key) == 0 {
@@ -136,26 +45,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	db, err = sql.Open("postgres", DbConnnect)
+	db, err := sql.Open("postgres", DbConnnect)
 	defer db.Close()
 	checkError(err)
 
-	deleteExisting()
+	refreshAuctions(db, api_key)
 
-	latest := loadLatest()
-	files := fetchDumps(api_key)
-
-	for _, file := range files {
-		if file.LastModified > latest {
-			fetchAuctions(file)
-
-			err = db.QueryRow(`INSERT INTO auction_files (url, last_modified) VALUES ($1, $2) RETURNING id`,
-				file.Url, file.LastModified).Scan(&fileId)
-			checkError(err)
-		} else {
-			log.Printf("Skipping. File too old: %v\n", file.Url)
-		}
-	}
+	ids := loadItemIds(db)
+	log.Printf("Found %v unique items.", len(ids))
 
 	log.Println("Done.")
 }
