4638e89ecb5701aec34d215a348c87d486127aca
open_mailbox <games@open-mailbox.com>
2018-08-10 14:39:09 -0400

    Better logging for the http server.

--- server.go
+++ server.go
@@ -3,12 +3,17 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 const localAddress = ":8081"
+const logFile = "log/server.log"
 
 var db *sql.DB
 
@@ -17,8 +22,6 @@ func enableCors(w *http.ResponseWriter) {
 }
 
 func handleNameSearch(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v / from  %v with params: %v\n", r.Method, r.RemoteAddr, r.URL.Query())
-
 	searchTermParam := r.URL.Query()["search"]
 
 	if len(searchTermParam) == 0 {
@@ -54,9 +57,7 @@ func handleNameSearch(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Completed %v %v\n", http.StatusOK, http.StatusText(http.StatusOK))
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v / from  %v with params: %v\n", r.Method, r.RemoteAddr, r.URL.Query())
-
+func handleHistory(w http.ResponseWriter, r *http.Request) {
 	itemIDParam := r.URL.Query()["itemId"]
 
 	if len(itemIDParam) == 0 {
@@ -69,7 +70,7 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 
 	lookupID := itemIDParam[0]
 
-	rows, err := db.Query(`SELECT periods.item_id, name, high, low, volume, open, close, created_at FROM periods 
+	rows, err := db.Query(`SELECT periods.item_id, name, high, low, volume, open, close, created_at FROM periods
 		INNER JOIN items on items.item_id = periods.item_id
 		WHERE periods.item_id = $1 ORDER BY periods.id DESC`, lookupID)
 	checkError(err)
@@ -101,11 +102,45 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Completed %v %v\n", http.StatusOK, http.StatusText(http.StatusOK))
 }
 
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v %v from %v\n", r.Method, r.URL, r.RemoteAddr)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func openLogFile(fileName string) {
+	if len(fileName) == 0 {
+		return
+	}
+
+	path, err := filepath.Abs(fileName)
+	checkError(err)
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
+
+	if err != nil {
+		log.Fatal("Can't open log file.", err)
+	}
+
+	mw := io.MultiWriter(os.Stdout, file)
+
+	log.SetOutput(mw)
+}
+
 func startServer(database *sql.DB) {
 	db = database
+
+	openLogFile(logFile)
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/history", handleRequest)
+	http.HandleFunc("/history", handleHistory)
 	http.HandleFunc("/names", handleNameSearch)
+
+	fmt.Printf("Logging to %v\n", logFile)
 	log.Printf("Listening on %v\n", localAddress)
-	log.Fatal(http.ListenAndServe(localAddress, nil))
+
+	log.Fatal(http.ListenAndServe(localAddress, logRequest(http.DefaultServeMux)))
 }
